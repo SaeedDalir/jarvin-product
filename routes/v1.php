0801138fc1a7501a9b7c016051f1155a3328a3fb
saeeddalir <saeedkingdr@gmail.com>
2020-01-04 15:02:49 +0330

    disable auth middleware temporarily

--- routes/v1.php
+++ routes/v1.php
@@ -2,7 +2,7 @@
 $router->group(['prefix' => 'v1' , 'namespace' => 'V1'] , function () use ($router) {
 
     /*  >>>>>>>>>>>>>>>>>>>>>>>>>>>> Products <<<<<<<<<<<<<<<<<<<<<<<<<<<<  */
-    $router->group(['middleware' => 'auth'] , function () use ($router) {
+//    $router->group(['middleware' => 'auth'] , function () use ($router) {
         $router->get('/products', 'ProductController@index');
         $router->get('/products/create', 'ProductController@create');
         $router->post('/products', 'ProductController@store');
@@ -10,7 +10,7 @@ $router->group(['prefix' => 'v1' , 'namespace' => 'V1'] , function () use ($rout
         $router->patch('/products/{product}', 'ProductController@update');
         $router->delete('/products/{product}', 'ProductController@destroy');
         $router->get('/products/status/{product}', 'ProductController@status');
-    });
+//    });
     /*  >>>>>>>>>>>>>>>>>>>>>>>>>>>> Products <<<<<<<<<<<<<<<<<<<<<<<<<<<<  */
 
 });
