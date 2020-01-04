63d0179894c13420e31ea172b5160d51cabc3251
saeeddalir <saeedkingdr@gmail.com>
2020-01-04 16:42:25 +0330

    write a mutator or setter function for persian-name

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,9 +1,12 @@
 <?php namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'products';
     protected $fillable = [
         'persian_name','english_name','store_id',
@@ -11,4 +14,9 @@ class Product extends Model
         'confirmation_status','in_stock','warranty_name',
         'warranty_text','current_price','view_count','comment_count'
     ];
+
+    public function setPersianNameAttribute($value): void
+    {
+        $this->attributes['persian_name'] = str_replace('ي', 'ی', $value);
+    }
 }
