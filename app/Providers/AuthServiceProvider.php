b5d648ee357da7e4892eb57b49d05c31bb4dc5ea
saeeddalir <saeedkingdr@gmail.com>
2020-01-04 14:20:20 +0330

    change authorize type for getting api_token just from header and validate it not by request

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,9 +31,11 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            $header = $request->header('api_token');
+            if ($header) {
+                return User::where('api_token', $header)->first();
             }
+            return null;
         });
     }
 }
