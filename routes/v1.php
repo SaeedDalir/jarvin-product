69395681695777997191c0b8966e0605c635f738
saeeddalir <saeedkingdr@gmail.com>
2020-01-05 16:55:44 +0330

    modify some routes

--- routes/v1.php
+++ routes/v1.php
@@ -5,8 +5,8 @@ $router->group(['prefix' => 'v1' , 'namespace' => 'V1'] , function () use ($rout
 //    $router->group(['middleware' => 'auth'] , function () use ($router) {
         $router->get('/products', 'ProductController@index');
         $router->post('/products', 'ProductController@store');
-        $router->get('/products/{product}/edit', 'ProductController@edit');
         $router->patch('/products/{product}', 'ProductController@update');
+        $router->get('/products/{product}', 'ProductController@show');
         $router->delete('/products/{product}', 'ProductController@destroy');
         $router->get('/products/status/{product}', 'ProductController@status');
 //    });
